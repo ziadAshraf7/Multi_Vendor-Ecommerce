c64cf5cc6b782d0f3cab86a1f0c3995d6d2bb870
ziadAshraf7 <ziadashraf454@icloud.com>
2024-09-10 21:22:10 +0300

    Initial project setup , Configured database connection , Set up Flyway for database migrations

---
+++ src/main/java/com/example/ecommerce_app/MultiVendorEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiVendorEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiVendorEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce_app/MultiVendorEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiVendorEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
