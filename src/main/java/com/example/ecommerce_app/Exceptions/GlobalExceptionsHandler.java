d034bc67445afc98d9b7f0cf78856c6396e36230
ziadAshraf7 <ziadashraf454@icloud.com>
2024-10-16 21:07:28 +0300

    feat: Enhance GlobalExceptionHandler for more granular exception handling

--- src/main/java/com/example/ecommerce_app/Exceptions/GlobalExceptionsHandler.java
+++ src/main/java/com/example/ecommerce_app/Exceptions/GlobalExceptionsHandler.java
@@ -3,10 +3,7 @@ package com.example.ecommerce_app.Exceptions;
 
 import com.example.ecommerce_app.Exceptions.ErrorResponses.ErrorResponse;
 import com.example.ecommerce_app.Exceptions.ErrorResponses.ValidationErrorResponse;
-import com.example.ecommerce_app.Exceptions.Exceptions.BadRequestException;
-import com.example.ecommerce_app.Exceptions.Exceptions.CustomRuntimeException;
-import com.example.ecommerce_app.Exceptions.Exceptions.NotFoundException;
-import jakarta.validation.ValidationException;
+import com.example.ecommerce_app.Exceptions.Exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -22,16 +19,23 @@ public class GlobalExceptionsHandler {
 
 
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
+    @ExceptionHandler(CustomBadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequestException(CustomBadRequestException ex) {
         return new ResponseEntity<>(
-                new ErrorResponse(ex.getMessage() , HttpStatus.NOT_FOUND.value()) , HttpStatus.NOT_FOUND
+                new ErrorResponse(ex.getMessage() , HttpStatus.BAD_REQUEST.value()) , HttpStatus.BAD_REQUEST
         );
     }
 
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
+    @ExceptionHandler(ImageNumberExceededException.class)
+    public ResponseEntity<ErrorResponse> handleImageNumberExceededException(ImageNumberExceededException ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse(ex.getMessage() , HttpStatus.BAD_REQUEST.value()) , HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(CustomNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFoundException(CustomNotFoundException ex) {
         return new ResponseEntity<>(
                 new ErrorResponse(ex.getMessage() , HttpStatus.NOT_FOUND.value()) , HttpStatus.NOT_FOUND
         );
@@ -45,6 +49,26 @@ public class GlobalExceptionsHandler {
         );
     }
 
+
+
+    @ExceptionHandler(DatabaseInternalServerError.class)
+    public ResponseEntity<ErrorResponse> handleDatabaseRuntimeExceptions(DatabaseInternalServerError ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse(ex.getMessage() , HttpStatus.INTERNAL_SERVER_ERROR.value()) , HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(DatabasePersistenceException.class)
+    public ResponseEntity<ErrorResponse> handleDatabasePersistenceExceptions(DatabasePersistenceException ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse(ex.getMessage() , HttpStatus.INTERNAL_SERVER_ERROR.value()) , HttpStatus.INTERNAL_SERVER_ERROR  );
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrity(DataIntegrityViolationException ex) {
+        return new ResponseEntity<>(
+                new ErrorResponse(ex.getMessage() , HttpStatus.CONFLICT.value()) , HttpStatus.CONFLICT  );
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
